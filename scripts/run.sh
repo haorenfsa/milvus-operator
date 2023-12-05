965986ed0fc3a31d623ad492890c361d2418373e
shaoyue.chen <shaoyue.chen@zilliz.com>
2023-12-05 17:22:27 +0800

    Support set oom_adj or oom_score_adj through env
    Signed-off-by: shaoyue.chen <shaoyue.chen@zilliz.com>

--- scripts/run.sh
+++ scripts/run.sh
@@ -7,5 +7,15 @@ MilvusHookConfigUpdatesMountPath="/milvus/configs/hook_updates.yaml"
 # merge config
 /milvus/tools/merge -s ${MilvusUserConfigMountPath} -d ${MilvusOriginalConfigPath}
 /milvus/tools/merge -s ${MilvusHookConfigUpdatesMountPath} -d ${MilvusHookConfigMountPath}
+# ajust oom_adj if env OOM_ADJ is set
+if [ -n "$OOM_ADJ" ]; then
+    echo "oom_adj is set to $OOM_ADJ"
+    echo "$OOM_ADJ" > /proc/$$/oom_adj
+fi
+# ajust oom_score_adj if env OOM_SCORE_ADJ is set
+if [ -n "$OOM_SCORE_ADJ" ]; then
+    echo "oom_score_adj is set to $OOM_SCORE_ADJ"
+    echo "$OOM_SCORE_ADJ" > /proc/$$/oom_score_adj
+fi
 # run commands
 exec $@
