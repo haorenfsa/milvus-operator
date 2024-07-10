e4fb683e2cee4209c56b3f33849975becaf28fe6
haorenfsa <shaoyue.chen@zilliz.com>
2024-07-10 16:09:59 +0800

    Fix some bugs when change to two deploy mode
    deploy_ctrl: fix changing deployMode
    
    Signed-off-by: haorenfsa <shaoyue.chen@zilliz.com>
    
    podtemplate defaults should be updated when stopped
    
    Signed-off-by: haorenfsa <shaoyue.chen@zilliz.com>
    
    Fix manual mode
    
    Signed-off-by: haorenfsa <shaoyue.chen@zilliz.com>

--- pkg/controllers/deploy_mode_changer.go
+++ pkg/controllers/deploy_mode_changer.go
@@ -34,6 +34,7 @@ func NewDeployModeChanger(component MilvusComponent, cli client.Client, util K8s
 		newStep("update old pod labels", c.UpdateOldPodLabels),
 		newStep("recover replica sets", c.RecoverReplicaSets),
 		newStep("recover deploy", c.RecoverDeploy),
+		newStep("mark current deploy", c.MarkCurrentDeploy),
 	}
 	return c
 }
@@ -150,11 +151,12 @@ func (c *DeployModeChangerImpl) RecoverReplicaSets(ctx context.Context, mc v1bet
 		rs.UID = ""
 		rs.ResourceVersion = ""
 		splitedName := strings.Split(rs.Name, "-")
-		if len(splitedName) < 2 {
+		if len(splitedName) < 3 {
 			return errors.Errorf("invalid old replica set name: %s", rs.Name)
 		}
 		rsHash := splitedName[len(splitedName)-1]
-		rs.Name = fmt.Sprintf("%s-0-%s", rs.Name, rsHash)
+		deployName := strings.Join(splitedName[:len(splitedName)-2], "-")
+		rs.Name = fmt.Sprintf("%s-0-%s", deployName, rsHash)
 		logger.Info("recovering old replica set", "new-name", rs.Name)
 		err = c.util.CreateObject(ctx, &rs)
 		if err != nil {
@@ -186,6 +188,12 @@ func (c *DeployModeChangerImpl) RecoverDeploy(ctx context.Context, mc v1beta1.Mi
 		return errors.Wrap(err, "recover old deploy")
 	}
 	return nil
+}
+
+func (c *DeployModeChangerImpl) MarkCurrentDeploy(ctx context.Context, mc v1beta1.Milvus) error {
+	v1beta1.Labels().SetCurrentGroupID(&mc, c.component.Name, 0)
+	err := c.util.UpdateAndRequeue(ctx, &mc)
+	return errors.Wrap(err, "mark current deploy")
 }
 
 func formatSaveOldDeployName(mc v1beta1.Milvus, component MilvusComponent) string {
