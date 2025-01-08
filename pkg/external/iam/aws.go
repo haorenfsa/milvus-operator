f74977a573d3b1e8ee8d11020c9d98dec2acbcec
haorenfsa <shaoyue.chen@zilliz.com>
2025-01-08 10:31:04 +0800

    fix aws iam
    Signed-off-by: haorenfsa <shaoyue.chen@zilliz.com>

--- pkg/external/iam/aws.go
+++ pkg/external/iam/aws.go
@@ -2,6 +2,7 @@ package iam
 
 import (
 	"context"
+	"strings"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
@@ -13,10 +14,21 @@ func VerifyAWS(ctx context.Context, bucketName, address string, secure bool) err
 	client, err := minio.New(address, &minio.Options{
 		Creds:  credentials.NewIAM(""),
 		Secure: secure,
+		Region: parseRegionByAddr(address),
 	})
 	if err != nil {
 		return errors.Wrap(err, "init minio client failed")
 	}
 	_, err = client.BucketExists(ctx, bucketName)
 	return errors.Wrapf(err, "access aws bucket[%s] failed", bucketName)
+}
+
+func parseRegionByAddr(address string) string {
+	// address format: s3.us-west-1.amazonaws.com:443
+	// region: us-west-1
+	splited := strings.Split(address, ".")
+	if len(splited) < 4 {
+		return ""
+	}
+	return splited[1]
 }
